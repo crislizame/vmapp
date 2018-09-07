6b5b6bc3b3dd9871ad571292101491be9d4496b1
Cristopher Lizame <boxgamebig@gmail.com>
2018-09-06 21:40:09 -0500

    asd

--- ws/funcion/function.productos.php
+++ ws/funcion/function.productos.php
@@ -31,31 +31,31 @@ class Productos
         switch ($tipo) {
             case 'stocka': $sql = 'SELECT p.artdescri as nombre, p.artstock as sotck, p.artprecventa1 as precio, l.lindescrip as line FROM producto p, linproducto l where l.lincodigo = p.lincodigo ASC;';
             $query = $db->sql($sql);
-            $row = $query->fetch_array(MYSQLI_ASSOC);
+            $row[] = $query->fetch_array(MYSQLI_ASSOC);
             return $row;
 
             break;
             case 'stockd': $sql = 'SELECT p.artdescri as nombre, p.artstock as sotck, p.artprecventa1 as precio, l.lindescrip as line FROM producto p, linproducto l where l.lincodigo = p.lincodigo DESC;';
             $query = $db->sql($sql);
-            $row = $query->fetch_array(MYSQLI_ASSOC);
+            $row[] = $query->fetch_array(MYSQLI_ASSOC);
             return $row;
 
             break;
             case 'all': $sql = 'SELECT p.artdescri as nombre, p.artstock as sotck, p.artprecventa1 as precio, l.lindescrip as line FROM producto p, linproducto l where l.lincodigo = p.lincodigo;';
             $query = $db->sql($sql);
-            $row = $query->fetch_array(MYSQLI_ASSOC);
+            $row[] = $query->fetch_array(MYSQLI_ASSOC);
             return $row;
 
             break;
             case 'bs': $sql = 'select pd.artcodigo as artcodigo, sum(pd.pedcantidad),p.artdescri as nombre, p.artstock as sotck, p.artprecventa1 as precio, l.lindescrip from pedidos_detalle pd, producto p, linproducto l where pd.artcodigo = p.artcodigo and p.lincodigo = l.lincodigo GROUP BY artcodigo order by sum(pd.pedcantidad) DESC;';
             $query = $db->sql($sql);
-            $row = $query->fetch_array(MYSQLI_ASSOC);
+            $row[] = $query->fetch_array(MYSQLI_ASSOC);
             return $row;
 
             break;
             case 'line': $sql = 'SELECT p.artdescri as nombre, p.artstock as sotck, p.artprecventa1 as precio, l.lindescrip as line FROM producto p, linproducto l where l.lincodigo = p.lincodigo;';
             $query = $db->sql($sql);
-            $row = $query->fetch_array(MYSQLI_ASSOC);
+            $row[] = $query->fetch_array(MYSQLI_ASSOC);
             return $row;
 
             break;
